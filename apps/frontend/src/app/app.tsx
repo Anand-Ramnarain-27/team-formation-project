f577456e52427866338505214bb2234d71ac2ad4
Anand-Ramnarain-27 <anandram0207@gmail.com>
2025-02-09 19:42:10 +0200

    Changed folder structure

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes, Link, Navigate } from 'react-router-dom';
 import '@/app/styles/global.css'
-import LoginPage from '@/app/pages/login/LoginPage';
+import LoginPage from '@/app/shared/pages/LoginPage';
 
 export function App() {
   return (
