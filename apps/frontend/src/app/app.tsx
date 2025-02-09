8a8091839ef2313c25bc3218ba22d2f03c14f049
Anand-Ramnarain-27 <anandram0207@gmail.com>
2025-02-09 20:07:52 +0200

    Added Student & Admin dashboard and redirects when loggingin

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,13 +1,17 @@
 import { Route, Routes, Link, Navigate } from 'react-router-dom';
 import '@/app/styles/global.css'
 import LoginPage from '@/app/shared/pages/LoginPage';
+import AdminDashboard from '@/app/admin/pages/Dashboard';
+import StudentDashboard from '@/app/student/pages/Dashboard'
 
 export function App() {
   return (
-    <div>
-      <LoginPage />
-
-    </div>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/admin" element={<AdminDashboard />} />
+      <Route path="/student" element={<StudentDashboard />} />
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
   );
 }
 
--- apps/frontend/src/app/shared/pages/LoginPage.tsx
+++ apps/frontend/src/app/shared/pages/LoginPage.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './LoginPage.module.css';
 import logo from '@/assets/logo/Team_formation-removebg.png'
 import GithubIcon from '@/assets/icons/Github-icon.svg'
 
 const LoginPage: React.FC = () => {
+  const navigate = useNavigate();
+
+  const isAdmin = true; 
+
+  const handleLogin = () => {
+    if (isAdmin) {
+      navigate('/admin'); 
+    } else {
+      navigate('/student');
+    }
+  };
+
   return (
     <div className={styles.loginContainer}>
       <img
@@ -11,7 +24,7 @@ const LoginPage: React.FC = () => {
         alt="Team Formation Logo"
         className={styles.loginImage}
       />
-      <button className={styles.loginButton} >
+      <button className={styles.loginButton} onClick={handleLogin} >
       <img src={GithubIcon} alt="GitHub Icon" className={styles.githubIcon} />
         Login With Github</button>
     </div>
